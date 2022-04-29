d0947673d0f652336bdef9a03b521e368c2b90c2
ueeil <ueeil@student.kit.edu>
2022-04-29 13:19:34 +0200

    update

--- web-app/nuxt.config.js
+++ web-app/nuxt.config.js
@@ -8,7 +8,7 @@ export default {
   target: 'static',
 
   router: {
-    base: 'https://github.com/ueeil/uebung/'
+    base: '/uebung/'
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
