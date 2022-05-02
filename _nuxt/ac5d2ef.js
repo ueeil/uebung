b1acef6a480ed639f145e7c655903f5139a29938
ueeil <ueeil@student.kit.edu>
2022-05-02 12:26:57 +0200

    9d2049c

---
+++ _nuxt/ac5d2ef.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{497:function(n,t,e){"use strict";e.r(t);var l=e(84),component=Object(l.a)({},(function(){var n=this.$createElement,t=this._self._c||n;return t("div",[t("tab-2")],1)}),[],!1,null,null,null);t.default=component.exports}}]);
